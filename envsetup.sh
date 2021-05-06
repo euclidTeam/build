024e25729639771599f63dc633419b4d99d31b6b
Michael Bestas <mkbestas@gmail.com>
2021-05-06 16:44:13 +0300

    envsetup: Set INLINE_KERNEL_BUILDING if TARGET_PREBUILT_KERNEL is set
    Change-Id: I2404adf572bdafb91edc66a19a83a2696004b33f

--- envsetup.sh
+++ envsetup.sh
@@ -885,6 +885,13 @@ function lunch()
       unset INLINE_KERNEL_BUILDING
     fi
 
+    local prebuilt_kernel=$(get_build_var TARGET_PREBUILT_KERNEL)
+    if [ -z "$prebuilt_kernel" ]; then
+      export INLINE_KERNEL_BUILDING=true
+    else
+      unset INLINE_KERNEL_BUILDING
+    fi
+
     [[ -n "${ANDROID_QUIET_BUILD:-}" ]] || echo
 
     fixup_common_out_dir
