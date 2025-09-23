AxionAOSP/android_build

envsetup: lunch: Generate build-manifest.xml after roomservice finishes
Change-Id: I3717820be4f40e74c2361dffff3f4c0103c72d70

envsetup.sh
@@ -600,6 +600,9 @@ function lunch()
         cd - > /dev/null
     fi
 
+    # Generate build-manifest.xml after roomservice finishes
+    vendor/lineage/build/tools/gen_build-manifest.sh
+
     # Validate the selection and set all the environment stuff
     _lunch_meat $product $release $variant
 }

euclidTeam/build

envsetup.sh
@@ -600,6 +600,9 @@ function lunch()
         cd - > /dev/null
     fi
 
+    # Generate build-manifest.xml after roomservice finishes
+    vendor/euclid/build/tools/gen_build-manifest.sh
+
     # Validate the selection and set all the environment stuff
     _lunch_meat $product $release $variant
 }